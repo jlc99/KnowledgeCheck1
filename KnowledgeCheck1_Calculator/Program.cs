cb5bab04f724b14e38ffd2cc3f8b0ce1eebff7c4
jlc99 <143123350+jlc99@users.noreply.github.com>
2023-11-12 21:21:53 -0500

    Update Program.cs

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -51,7 +51,18 @@ namespace KnowledgeCheck1_Calculator
                     break;
 
                 case "3":
-                    // Add code here
+                    Console.WriteLine("Enter 2 integers to multiply");
+                    var multiplyNumber1 = Console.ReadLine();
+                    var multiplyNumber2 = Console.ReadLine();
+
+                    if (double.TryParse(Numbermultiply1, out double multplyNumOne) && multiply.TryParse(multiplyNumber2, out double multNumTwo))
+                    {
+                        Console.Write($"{multiplyNumber1} / {multiplyNumber2} = ");
+                        Console.Write(calculator.multiply(prodNumOne, produNumTwo));
+                    }
+                    else
+                    {
+                        Console.WriteLine("One or more of the numbers is not an int");
                     break;
 
                 case "4":
